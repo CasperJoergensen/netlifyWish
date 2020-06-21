c572a42f194c4a6c878b13fa0e6b5132b377e6a5
Casper <jorgensen2603@gmail.com>
2020-06-21 15:03:39 +0200

    lambda

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
     console.log('queryStringParameters', event.queryStringParameters)
-    callback(null, {
+    return callback(null, {
         statusCode: 200,
         body: JSON.stringify({ msg: 'Hello, World!' }),
     })
